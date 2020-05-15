fd99c2aac8b697be0aca9da4c67210ec7e777995
Sal E <theking@Sals-MacBook-Pro.local>
2020-05-14 20:49:55 -0400

    Added module 1 assignment to repo

---
+++ src/module_1/App.java
@@ -0,0 +1,55 @@
+//Author Name: Sally Ellison
+//Date: 5/14/20
+//Program Name: Ellison_Drone
+//Purpose: Simulation using button, drone movement in x, y,z location
+
+package module_1;
+
+import java.util.Scanner;
+
+public class App
+{
+    private static Scanner scanner = new Scanner(System.in);
+    private static Ellison_Drone drone = new Ellison_Drone();
+    
+    public static void main(String args[])
+    {
+        boolean control_bool = true;
+        while (control_bool)
+        {
+            printMenu();
+            
+            int choice = scanner.nextInt();
+            
+            switch (choice)
+            {
+                case 1 : drone.moveUp();          break;
+                case 2 : drone.moveDown();        break;
+                case 3 : drone.moveForward();     break;
+                case 4 : drone.moveDown();        break;
+                case 5 : drone.turnLeft();        break;
+                case 6 : drone.turnRight();       break;
+                case 7 : drone.displayPosition(); break;
+                case 8 : control_bool = false;    break;
+                default :
+                    
+            }
+            
+        }
+        
+        System.out.println("Exiting navigation");
+    }
+    
+    private static void printMenu()
+    {
+        System.out.println("Which direction would you like to move the drone?");
+        System.out.println("1 - Move Up");
+        System.out.println("2 - Move Down");
+        System.out.println("3 - Move Forward");
+        System.out.println("4 - Move Backward");
+        System.out.println("5 - Turn Left");
+        System.out.println("6 - Turn Right");
+        System.out.println("7 - Display Position");
+        System.out.println("8 - Exit Navigation");
+    }
+}
---
+++ src/module_1/Ellison_Drone.java
@@ -0,0 +1,93 @@
+//Author Name: Sally Ellison
+//Date: 5/14/20
+//Program Name: Ellison_Drone
+//Purpose: Simulation using button, drone movement in x, y,z location
+
+package module_1;
+
+public class Ellison_Drone
+{
+    public enum Orientation { NORTH, SOUTH, EAST, WEST }
+    
+    private int x = 0;
+    private int y = 0;
+    private int z = 0;
+    private Orientation orientation = Orientation.NORTH;
+    
+    public void moveUp()
+    {
+        z++;
+        System.out.println("You have moved up");
+    }
+    
+    public void moveDown()
+    {
+        z--;
+        System.out.println("You have moved down");
+    }
+    
+    public void moveForward()
+    {
+        switch(orientation)
+        {
+          case NORTH : y++; break;
+          case SOUTH : y--; break;
+          case EAST  : x++; break;
+          case WEST  : x--; break;
+          default : 
+        }
+        System.out.println("You have moved forward");
+    }
+    
+    public void moveBackward()
+    {
+        switch(orientation)
+        {
+          case NORTH : y--; break;
+          case SOUTH : y++; break;
+          case EAST  : x--; break;
+          case WEST  : x++; break;
+          default : 
+        }
+        
+        System.out.println("You have moved backward");
+    }
+    
+    public void turnLeft()
+    {
+        switch(orientation)
+        {
+          case NORTH : orientation = Orientation.WEST; break;
+          case SOUTH : orientation = Orientation.EAST; break;
+          case EAST  : orientation = Orientation.NORTH; break;
+          case WEST  : orientation = Orientation.SOUTH; break;
+          default : 
+        }
+        
+        System.out.println("You have turned left");
+    }
+    
+    public void turnRight()
+    {
+        switch(orientation)
+        {
+          case NORTH : orientation = Orientation.EAST; break;
+          case SOUTH : orientation = Orientation.WEST; break;
+          case EAST  : orientation = Orientation.SOUTH; break;
+          case WEST  : orientation = Orientation.NORTH; break;
+          default : 
+        }
+        
+        System.out.println("You have turned right");
+    } 
+    
+    public void displayPosition()
+    {
+        String positionInfo = String.format("Ellison_Drone [x_pos=%d, y_pos=%d, z_pos=%d, orientation=%s]", x, y, z, orientation);
+        System.out.println(positionInfo);
+    }
+    
+    
+    
+    
+}
